42fa951459dfb21993219dd6dbb18e50285a1e8f
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2022-10-10 12:44:57 +0200

    Añadido tiempo de vida y la opción de poder destruir los proyectiles

--- Assets/Code/Projectiles/SO/ProjectileConfigurationSO.cs
+++ Assets/Code/Projectiles/SO/ProjectileConfigurationSO.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 public class ProjectileConfigurationSO : ScriptableObject
 {
     [SerializeField] private GameObject _prefab;
+    [SerializeField] private float _maximumLifetime = 5f;
     [SerializeField] private ProjectileMovementSO _movementType;
     [SerializeField] private ProjectileDamagerSO _damageType;
 
     public GameObject Prefab => _prefab;
+    public float MaximumLifetime => _maximumLifetime;
     public IProjectileMovement Movement => _movementType.Movement;
     public IProjectileDamager Damager => _damageType.Damager;
 }
