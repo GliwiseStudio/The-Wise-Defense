d4d75b0ce7d3d52fafbdc1996555ca45c3a9c15e
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2022-10-24 11:05:23 +0200

    Añadido sonido al spawnear carta

--- Assets/Code/cards/SO/Cards/CardConfigurationSO.cs
+++ Assets/Code/cards/SO/Cards/CardConfigurationSO.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 public class CardConfigurationSO : ScriptableObject
 {
     public CardPowerConfigurationSO cardPower;
+    [SerializeField] private string _activationSoundName;
+    [SerializeField] private AudioPlayer _audioPlayerPrefab;
     [SerializeField] private GameObject _blueprintPrefab;
     [SerializeField] private string[] _spawnLayers;
 
     public bool InGameCard;
 
+    public string ActivationSoundName => _activationSoundName;
+    public AudioPlayer AudioPlayerPrefab => _audioPlayerPrefab;
     public GameObject BlueprintPrefab => _blueprintPrefab;
     public string[] SpawnLayers => _spawnLayers;
 
