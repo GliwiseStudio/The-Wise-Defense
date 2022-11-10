ce8a3d8bf15fefe5d79b7f7f77169153f8e82e86
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2022-11-10 09:03:18 +0100

    Inventory UI

---
+++ Assets/Code/Inventory/InventoryController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class InventoryController : MonoBehaviour
+{
+    private RectTransform _cardRowsHolder;
+}
