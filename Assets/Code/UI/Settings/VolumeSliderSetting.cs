260ef95c0e76f632cded0de85d4c60426e8de77c
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2022-11-10 12:49:06 +0100

    sdd

--- Assets/Code/UI/Settings/VolumeSliderSetting.cs
+++ Assets/Code/UI/Settings/VolumeSliderSetting.cs
@@ -8,24 +8,27 @@ public class VolumeSliderSetting : MonoBehaviour
     [SerializeField] private Slider _slider;
     [SerializeField] private TextMeshProUGUI _nameText;
     [SerializeField] private TextMeshProUGUI _valueText;
-    [SerializeField][Range(0, 100)] private float _initialValue = 100f;
+    [SerializeField][Range(0, 1)] private float _initialValue = 1f;
     private float _minimumValue = 0.0001f;
     private float _maximumValue = 1f;
     [SerializeField] private string _settingText = "Default";
     [SerializeField] private string _audioMixerName = "Master";
+    [SerializeField] private string _volumeParameterName = "MasterVolume";
     private AudioMixerGroupController _audioMixerGroupController;
 
     private void Awake()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
     {
         _audioMixerGroupController = FindObjectOfType<AudioMixerGroupController>();
         _nameText.text = _settingText;
         _slider.minValue = _minimumValue;
         _slider.maxValue = _maximumValue;
-    }
 
-    private void Start()
-    {
-        _slider.value = (float)_initialValue / 100f;
+        ChangeValue(_initialValue);
     }
 
     private void OnEnable()
@@ -42,6 +45,7 @@ public class VolumeSliderSetting : MonoBehaviour
     private void ChangeValue(float newValue)
     {
         _valueText.text = ((int) (newValue * 100)).ToString();
-        _audioMixerGroupController.SetAudioMixerVolume(_audioMixerName, newValue);
+        _slider.value = newValue;
+        _audioMixerGroupController.SetAudioMixerVolume(_audioMixerName, _volumeParameterName, newValue);
     }
 }
