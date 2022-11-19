a876910342fd72a783aaee96bff8b28a268302f0
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2022-11-19 11:45:38 +0100

    Añadido sonido de level up

--- Assets/Code/Towers/SO/TowerAudioConfiguration.cs
+++ Assets/Code/Towers/SO/TowerAudioConfiguration.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class TowerAudioConfiguration
 {
     [SerializeField] private string _shotSound;
+    [SerializeField] private string _levelUpSound;
     [SerializeField] private string _audioMixerChannel = "SoundEffects";
 
     public string ShotSound => _shotSound;
+    public string LevelUpSound => _levelUpSound;
     public string AudioMixerChannel => _audioMixerChannel;
 }
