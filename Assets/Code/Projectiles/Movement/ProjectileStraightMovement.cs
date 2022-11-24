7e48343dbafe74e61ec13179de900a2b1d798bcc
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2022-11-24 16:32:03 +0100

    Bug de bala no siendo destruida al terminar su trayectoria solucionado

--- Assets/Code/Projectiles/Movement/ProjectileStraightMovement.cs
+++ Assets/Code/Projectiles/Movement/ProjectileStraightMovement.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class ProjectileStraightMovement : IProjectileMovement
 {
-    private Vector3 _lastTargetPosition = Vector3.zero;
-
-    public Vector3 UpdateMovement(Transform projectileTransform, Collider targetCollider, float speed)
+    public Vector3 UpdateMovement(Vector3 currentPosition, Vector3 targetPosition, float speed)
     {
-        if(targetCollider != null)
-        {
-            _lastTargetPosition = targetCollider.bounds.center;
-        }
-
-        return Vector3.Lerp(projectileTransform.position, _lastTargetPosition, speed * Time.deltaTime);
+        return Vector3.Lerp(currentPosition, targetPosition, speed * Time.deltaTime);
     }
 }
