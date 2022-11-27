d156b2c459fe7bd50e048e16edfa9bec487f19e2
Daniel Jiménez Morales <64079694+DanielJimenezMorales@users.noreply.github.com>
2022-11-27 12:07:42 +0100

    Añadido objecto pool genérica y aplicada a las balas

---
+++ Assets/Code/ObjectPool/RecyclableObject.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public abstract class RecyclableObject : MonoBehaviour
+{
+    private ObjectPool _objectPool;
+
+    public void Configure(ObjectPool objectPool)
+    {
+        _objectPool = objectPool;
+    }
+
+    public void Recycle()
+    {
+        _objectPool.RecycleGameObject(this);
+    }
+
+    public abstract void Init();
+
+    public abstract void Release();
+}
