ac2cdf354da1ab08ef18085dd62e0603eb07700f
bdevainy <auro.g.r.14@gmail.com>
2022-12-11 10:16:25 +0100

    fixed x2 bug

--- Assets/Code/UI/IncreaseWaveSpeed.cs
+++ Assets/Code/UI/IncreaseWaveSpeed.cs
@@ -11,10 +11,12 @@ public class IncreaseWaveSpeed : MonoBehaviour
     private Button _button;
     public GameObject textx1;
     public GameObject textx2;
+    private int _speed = 1;
 
     private void Awake()
     {
         _button = GetComponent<Button>();
+        _speed = 1;
     }
     private void OnEnable()
     {
@@ -46,14 +48,21 @@ public class IncreaseWaveSpeed : MonoBehaviour
         if(_activateWaveSpeed == false)
         {
             Time.timeScale = 1;
+            _speed = 1;
             textx1.SetActive(true);
             textx2.SetActive(false);
         }
         else
         {
             Time.timeScale = 2;
+            _speed = 2;
             textx1.SetActive(false);
             textx2.SetActive(true);
         }
+    }
+
+    public int GetSpeed()
+    {
+        return _speed;
     }
 }
